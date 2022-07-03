a70a62b4bf4ec7ca1accd17a01de50bade8b5a33
Sannonthachai <sannonthachai.t@globish.co.th>
2022-07-03 21:55:47 +0700

    Setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,45 @@
 module.exports = {
   root: true,
   env: {
-  browser: true,
-  node: true,
+    browser: true,
+    node: true,
   },
   extends: [
-  'plugin:vue/vue3-recommended',
-  'eslint:recommended',
-  '@vue/typescript/recommended',
-  '@vue/prettier',
-  '@vue/prettier/@typescript-eslint',
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   plugins: ['@typescript-eslint'],
   rules: {
-  // override/add rules settings here, such as:
-  'max-len': [
-  'warn',
-  {
-  code: 100,
-  ignoreComments: true,
-  ignoreTrailingComments: true,
-  ignorePattern: '^\\s*import',
-  ignoreStrings: true,
-  ignoreTemplateLiterals: true,
-  },
-  ],
-  'vue/no-unused-vars': 'warn',
-  '@typescript-eslint/explicit-module-boundary-types': [
-  'off',
-  {
-  allowedNames: ['ignoredFunctionName', 'ignoredMethodName'],
-  },
-  ],
-  'vue/valid-v-model': 'off',
-  'vue/no-v-html': 'off',
-  quotes: ['warn', 'single'],
-  semi: ['warn', 'never'],
-  'vue/multi-word-component-names': 0,
+    // override/add rules settings here, such as:
+    'max-len': [
+      'warn',
+      {
+        code: 100,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignorePattern: '^\\s*import',
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    'vue/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': [
+      'off',
+      {
+        allowedNames: ['ignoredFunctionName', 'ignoredMethodName'],
+      },
+    ],
+    'vue/valid-v-model': 'off',
+    'vue/no-v-html': 'off',
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'never'],
+    'vue/multi-word-component-names': 0,
   },
   globals: {
-  defineProps: 'readonly',
-  defineEmits: 'readonly',
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
   },
-  }
-  
+}
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,29 @@
 <template>
-    <nav class="text-3xl font-bold">Dear Diary</nav>
-    <div class="flex flex-col items-center">
-        <form class="row m-auto px-3 py-3 bg-gray-100 rounded-md">
-            <h6 class="text-2xl font-bold mb-5">Log in</h6>
-            <label class="block">
-                <input type="text" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="username"> 
-            </label>  
-            <label class="block">
-                <input type="password" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="password"> 
-            </label>
-            <div class="col-12 p-3">
-                <button class="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-full">log in</button>  
-            </div>       
-        </form>
-    </div>
+  <nav class="text-3xl font-bold">Dear Diary</nav>
+  <div class="flex flex-col items-center">
+    <form class="row m-auto px-3 py-3 bg-gray-100 rounded-md">
+      <h6 class="text-2xl font-bold mb-5">Log in</h6>
+      <label class="block">
+        <input
+          type="text"
+          class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+          placeholder="username"
+        />
+      </label>
+      <label class="block">
+        <input
+          type="password"
+          class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+          placeholder="password"
+        />
+      </label>
+      <div class="col-12 p-3">
+        <button
+          class="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-full"
+        >
+          log in
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
