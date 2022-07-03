13eaa04da00421918ca674c5a3c477e3e801f604
apricotxapril <auay.smiley@icloud.com>
2022-07-04 03:02:48 +0700

    commit

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -18,27 +18,25 @@
   <body>
     <div class="flex mx-20 h-screen bg-gradient-to-r from-green-200 to-blue-300 rounded-3xl pb-10">
       <div class="w-1/2">
-        <h1 class="text-6xl text-start font-semibold pl-24 pt-52 pb-10">
+        <h1 class="text-6xl text-start font-semibold pl-28 pt-52 pb-10">
           Take a note everywhere with DearDiary
         </h1>
-        <p class="text-2xl text-start pl-24 pb-14 font-medium">
+        <p class="text-2xl text-start pl-28 pb-14 font-medium">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Massa tempor nec feugiat nisl pretium fusce id.
         </p>
-        <div class="flex justify-content-start pl-24">
+        <div class="flex justify-content-start pl-28">
           <button class="text-2xl text-white font-semibold bg-emerald-500 px-10 py-5 rounded-lg">
             Download
           </button>
         </div>
       </div>
       <div class="w-1/2">
-        <div class="px-20 pt-36 pb-10">
+        <div class="pr-16 pt-36 pb-10">
           <img src="@/assets/diary.png" alt="" />
         </div>
       </div>
     </div>
-
-    <div></div>
   </body>
   <footer>
     <div class="mx-20 my-3">footer</div>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,56 @@
 <template>
-  <nav class="text-3xl font-bold">Dear Diary</nav>
-  <div class="flex flex-col items-center">
-    <form class="row m-auto px-3 py-3 bg-gray-100 rounded-md">
-      <h6 class="text-2xl font-bold mb-5">Log in</h6>
-      <label class="block">
+  <div
+    class="flex items-center justify-center h-screen bg-gradient-to-tr from-green-200 to-blue-300"
+  >
+    <form class="space-y-8 px-32 py-28 bg-white rounded-md" action="#">
+      <h5 class="text-3xl mb-10 font-bold text-gray-900">Welcome!</h5>
+      <div>
+        <label for="email" class="block mb-2 text-md text-start font-medium text-gray-900"
+          >Your email</label
+        >
         <input
-          type="text"
-          class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-          placeholder="username"
+          type="email"
+          name="email"
+          id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="name@hotmail.com"
         />
-      </label>
-      <label class="block">
+      </div>
+      <div>
+        <label for="password" class="block mb-2 text-md text-start font-medium text-gray-900"
+          >Your password</label
+        >
         <input
           type="password"
-          class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-          placeholder="password"
+          name="password"
+          id="password"
+          placeholder="••••••••"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         />
-      </label>
-      <div class="col-12 p-3">
-        <button
-          class="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-full"
-        >
-          log in
-        </button>
+      </div>
+      <div class="flex items-start">
+        <div class="flex items-start">
+          <div class="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300"
+            />
+          </div>
+          <label for="remember" class="ml-2 text-sm font-medium text-gray-900">Remember me</label>
+        </div>
+        <a href="#" class="ml-auto text-sm text-blue-700 hover:underline">Forgot Password?</a>
+      </div>
+      <button
+        type="submit"
+        class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      >
+        Login to your account
+      </button>
+      <div class="text-sm font-medium text-gray-500">
+        Not registered?
+        <a href="#" class="text-blue-700 hover:underline">Create account</a>
       </div>
     </form>
   </div>
